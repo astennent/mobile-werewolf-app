4401e8de8f9f15a6a14b5f81d36b361beb498f14
astennent <tennent4@gmail.com>
2013-11-16 05:03:28 +0000

    Added migrations

---
+++ migrations/0005_auto__chg_field_game_start_time.py
@@ -0,0 +1,104 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+
+        # Changing field 'Game.start_time'
+        db.alter_column(u'wolves_game', 'start_time', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True))
+
+    def backwards(self, orm):
+
+        # Changing field 'Game.start_time'
+        db.alter_column(u'wolves_game', 'start_time', self.gf('django.db.models.fields.DateField')(auto_now_add=True))
+
+    models = {
+        u'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        u'auth.permission': {
+            'Meta': {'ordering': "(u'content_type__app_label', u'content_type__model', u'codename')", 'unique_together': "((u'content_type', u'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        u'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        u'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        u'wolves.account': {
+            'Meta': {'object_name': 'Account'},
+            'badges': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['wolves.Badge']", 'symmetrical': 'False', 'blank': 'True'}),
+            'experience': ('django.db.models.fields.PositiveIntegerField', [], {'default': '0'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'+'", 'to': u"orm['auth.User']"})
+        },
+        u'wolves.badge': {
+            'Meta': {'object_name': 'Badge'},
+            'description': ('django.db.models.fields.CharField', [], {'max_length': '127'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '63'}),
+            'points': ('django.db.models.fields.IntegerField', [], {})
+        },
+        u'wolves.game': {
+            'Meta': {'object_name': 'Game'},
+            'administrator': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'game_admin'", 'null': 'True', 'to': u"orm['auth.User']"}),
+            'cycle_length': ('django.db.models.fields.IntegerField', [], {'default': '720'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'in_progress': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'kill_range': ('django.db.models.fields.FloatField', [], {'default': '5.0'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '31', 'null': 'True', 'blank': 'True'}),
+            'scent_range': ('django.db.models.fields.FloatField', [], {'default': '10.0'}),
+            'start_time': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'})
+        },
+        u'wolves.kill': {
+            'Meta': {'object_name': 'Kill'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'killer': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'kill-killer'", 'to': u"orm['wolves.Player']"}),
+            'latitude': ('django.db.models.fields.FloatField', [], {}),
+            'longitude': ('django.db.models.fields.FloatField', [], {}),
+            'time': ('django.db.models.fields.DateField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'victim': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'kill-victim'", 'to': u"orm['wolves.Player']"})
+        },
+        u'wolves.player': {
+            'Meta': {'object_name': 'Player'},
+            'account': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['wolves.Account']"}),
+            'game': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['wolves.Game']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_dead': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_wolf': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'latitude': ('django.db.models.fields.FloatField', [], {'default': '0.0'}),
+            'longitude': ('django.db.models.fields.FloatField', [], {'default': '0.0'}),
+            'vote': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['wolves.Player']", 'null': 'True', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['wolves']
